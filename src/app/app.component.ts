22a7cbf173dc6eda720adf16ce0e6653933a33f6
naga <nagarajukondapalli8663@gmail.com>
2022-05-25 13:19:53 +0530

    student-data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  [x: string]: any;
   title = 'student-data-table';
+listData:any;
+
+
+  userForm:FormGroup
+  item: any;
+  constructor(private fb:FormBuilder){
+
+    this.listData=[];
+    this.userForm = this.fb.group({
+      name:['',Validators.required],
+      address:['',Validators.required],
+      contactNo:['',Validators.required],
+      gender:['',Validators.required]
+
+    })
+  }
+  addItem()
+  {
+    this.listData.push(this.userForm.value);
+
+  }
+  Reset(){
+    this.userForm.reset();
+  }
+  i:any;
+  removeItem(i:any)
+  {
+    delete this.listData[i]
+
+  }
+ 
 }
+
+
+
+
+
